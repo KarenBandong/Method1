6e0b7efdbe6e385b2b37cee36efb2455923b0bf1
Karen <karen.bandong.2018@student.lhschools.org>
2019-02-22 08:04:38 -0500

    Scanner and "Hello + Username" set up.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,15 +1,27 @@
 package com.LickingHeights;
 
+import javax.print.attribute.standard.JobOriginatingUserName;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner keyboard= new Scanner(System.in);
+        String Username;
 
         printGreetings("World");
+        System.out.println("Hi! What is your name?");
+        Username= keyboard.nextLine();
+        printHello(Username);
 
     }
             public static void printGreetings(String noun){
         System.out.println("Hello "+ noun);
+        }
+
+
+        public static void printHello(String Username){
+        System.out.println("Hello "+ Username);
         }
 
 

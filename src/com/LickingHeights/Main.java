dbadf26ba2ecdd169d09325dace1020d901f0650
Karen <karen.bandong.2018@student.lhschools.org>
2019-02-22 18:46:16 -0500

    Dividing given numbers using method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,36 +6,42 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner keyboard= new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
         String Username;
 
 
         printGreetings("World");
         System.out.println("Hi! What is your name?");
-        Username= keyboard.nextLine();
+        Username = keyboard.nextLine();
         printHello(Username);
-        int x= 20;
-        int y= 10;
-        System.out.println(multiplying(x,y));
-
+        int x = 20;
+        int y = 10;
+        double pValue = 100;
+        double zValue = 60;
+        System.out.println(multiplying(x, y));
+        System.out.println(division(pValue, zValue));
 
 
     }
-            public static void printGreetings(String noun){
-        System.out.println("Hello "+ noun);
-        }
+
+    public static void printGreetings(String noun) {
+        System.out.println("Hello " + noun);
+    }
 
 
-        public static void printHello(String Username){
+    public static void printHello(String Username) {
 
-        System.out.println("Hello "+ Username);
-        }
+        System.out.println("Hello " + Username);
+    }
 
-        public static  int multiplying(int x, int y){
-        return x*y;
+    public static int multiplying(int x, int y) {
+        return x * y;
+    }
 
-        
-        }
+    public static double division(double pValue, double zValue) {
+        return pValue/zValue;
+    }
+
 
 
     }
